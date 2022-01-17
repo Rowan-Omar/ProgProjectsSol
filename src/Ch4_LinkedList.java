7760e66b88de07eabf7d1f9c6b9e111ddfa1c96a
Rowan Omar <61714846+Rowan-Omar@users.noreply.github.com>
2022-01-17 02:24:45 +0200

    Uploading Ch4 and Ch6

---
+++ src/Ch4_LinkedList.java
@@ -0,0 +1,181 @@
+public class Ch4_LinkedList {
+   // Q. 2 // done
+    /* public static <E> void removeDuplicates(Node<E> head) {
+        Node<E> current = head, dummy, temp2;
+
+        while (current != null && current.getLink() != null) {
+            dummy = current;
+            temp2 = current.getLink();
+            while (temp2 != null) {
+                if (current.getData() == temp2.getData()) {
+                    dummy.setLink(temp2.getLink());
+                } else
+                    dummy = temp2;
+                temp2 = temp2.getLink();
+            }
+            current = current.getLink();
+        }
+    }*/
+    // Q. 3  // done
+   /* public static void nodesBetween(Node<Integer> head, int x, int y) {
+        Node<Integer> start = listSearch(head, x);
+        Node<Integer> end = listSearch(head, y);
+        if (start == null || end == null) return;
+
+        Node<Integer> ptr;
+        System.out.print("The elements between the two num are: ");
+        for (ptr = start; ptr != end; ptr = ptr.getLink())
+            System.out.print(ptr.getData() + " ");
+        System.out.println(ptr.getData());
+    }*/
+
+    //Q. 4 // done
+   /* public static <E> Node<E> reverseList(Node<E> head) {
+        if (head == null) return null;
+        Node<E> answer = listCopy(head);
+        Node<E> prev = null, current = answer, next = null;
+        while (current != null) {
+            next = current.link;
+            current.link = prev;
+            prev = current;
+            current = next;
+        }
+        answer = prev;
+        return answer;
+    }*/
+
+    //Q.5 // done
+   /* public static Node<Integer> merge(Node<Integer> h1, Node<Integer> h2) {
+        if (h1 == null || h2 == null) return null;
+        Node<Integer> answer = new Node<Integer>(-1, null);
+        Node<Integer> ptr = answer;
+        while (h1 != null && h2 != null) {
+            if (h1.getData() <= h2.getData()) {
+                ptr.link = h1;
+                h1 = h1.link;
+            } else {
+                ptr.link = h2;
+                h2 = h2.link;
+            }
+            ptr = ptr.link;
+        }
+        if (h1 == null)
+            ptr.link = h2;
+        else if (h2 == null)
+            ptr.link = h1;
+        return answer.link;
+
+      /*  //OR
+        Node<Integer> answer= new Node<Integer>();
+        if(h1 == null)
+            return h2;
+        else if(h2 == null)
+            return h1;
+
+        if(h1.getData() <= h2.getData()){
+            answer = h1;
+            answer.link = merge(h1.link, h2);
+        }
+        else {
+            answer = h2;
+            answer.link = merge(h1, h2.link);
+        }
+        return answer;//
+    }*/
+
+    //Q.6  //done
+   /* public static Node<Integer>[] listSplitter(Node<Integer> head, int splitterValue) {
+        Node<Integer>[] answer = new Node[2];
+        Node<Integer> cursor = head;
+        Node<Integer> dummyHead1 = new Node<Integer>(-1, null);
+        Node<Integer> dummyHead2 = new Node<Integer>(-1, null);
+        Node<Integer> tail1 = dummyHead1, tail2 = dummyHead2;
+
+        for (; cursor != null; cursor = cursor.link) {
+            if (cursor.data < splitterValue) {
+                tail1.addNodeAfter(cursor.data);
+                tail1 = tail1.link;
+            } else {
+                tail2.addNodeAfter(cursor.data);
+                tail2 = tail2.link;
+            }
+        }
+        answer[0] = dummyHead1.link;
+        answer[1] = dummyHead2.link;
+        display(answer[0]);
+        display(answer[1]);
+        return answer;
+    }*/
+
+    //Q. 7  // done
+   /* public static Node<Integer> listSort(Node<Integer> head) {
+        if (head == null) return null;
+        Node<Integer> current = head, next = null;
+        int temp; //for swapping
+
+        while (current != null) {
+            next = current.link;
+            while (next != null) {
+                if (current.data > next.data) {
+                    temp = current.data;
+                    current.data = next.data;
+                    next.data = temp;
+                }
+                next = next.link;
+            }
+            current = current.link;
+        }
+        return null;
+    }*/
+
+    //Q.8  // NOT done
+    /*  public void subtract(LinkedList<Integer> Y) {
+        LinkedList<Integer> temp;
+        temp = (LinkedList<Integer>) this.clone();
+        int max = -1, max2 = -1;
+
+        while (temp.head != null) {
+            if (max < temp.head.getData())
+                max = temp.head.getData();
+            temp.head = temp.head.getLink();
+        }
+        temp = (LinkedList<Integer>) this.clone();
+        while (temp.head != null) {
+            if (max2 < temp.head.getData())
+                max2 = temp.head.getData();
+            temp.head = temp.head.getLink();
+        }
+        int[] myArr = new int[max + 1];
+        int[] myArr2 = new int[max2 + 1];
+
+        temp = (LinkedList<Integer>) this.clone();
+
+        while (temp.head != null) {
+            if (myArr[temp.head.getData()] == 0)
+                myArr[temp.head.getData()] = temp.countOccurrences(temp.head.getData());
+
+            temp.head = temp.head.getLink();
+        }
+        LinkedList<Integer> temp2=(LinkedList<Integer>) this.clone();
+
+        while (temp2.head != null) {
+            if (myArr2[temp2.head.getData()] == 0)
+                myArr2[temp2.head.getData()] = temp2.countOccurrences(temp2.head.getData());
+
+            temp2.head = temp2.head.getLink();
+        }
+        for(int i=0; i <= (max<max2 ? max: max2); i++){
+            myArr[i] = myArr[i] - myArr[i];
+        }
+
+        LinkedList<Integer> newBag = new LinkedList<>();
+        for (int i=0; i<=max; i++){
+            for(int j = 1; j<=myArr[i]; j++){
+                if(myArr[i]>0)
+                    //addLast(myArr[i]);
+            }
+        }
+    }*/
+
+
+}
---
+++ src/Ch6_Stack.java
@@ -0,0 +1,346 @@
+
+public class Ch6_Stack {
+
+    // Q.2
+    /*public Object itemAt(int n) {
+        E x;
+        if (n < 0 || n > size()) throw new NoSuchElementException();
+
+        ArrayStack<E> temp = new ArrayStack<E>();
+        for (int i = 0; i < n; i++) {
+            temp.push((E) pop());
+        }
+        x = (E) peek();
+        while (!temp.isEmpty()) {
+            this.push((E) temp.pop());
+        }
+        return x;
+    }*/
+
+    // Q.5 // done  but without the enhancement part
+    /*class BooleanExpEval {
+        static String expression; //the exp entered from the user
+        public static final Pattern UNSIGNED_INT = Pattern.compile("\\d+.*?");
+        public static final Pattern CHARACTER = Pattern.compile("\\S.*?");
+
+        public static void main(String[] args) {
+            Scanner input = new Scanner(System.in);
+            //(((10>5)||(10>15))&&(15>=20))
+            do {
+                System.out.print("Enter your expression: ");
+                expression = input.nextLine();
+                evaluate(expression);
+                try {
+                } catch (Exception e) {
+                    System.out.println("The stack is empty");
+                }
+            } while (Objects.equals(expression, "Exit")); //it is not looping
+        }
+
+        public static Boolean evaluate(String exp) {
+            Stack<Integer> num = new Stack<>();
+            Stack<Boolean> bool = new Stack<>();
+            Stack<Character> opStack = new Stack<>();
+            exp = exp.replace(">=", "G");
+            exp = exp.replace("<=", "L");
+            exp = exp.replace("==", "E");
+            exp = exp.replace("!=", "N");
+            exp = exp.replace("||", "O");
+            exp = exp.replace("&&", "A");
+            Scanner ex = new Scanner(exp);
+            String next;
+            char c;
+            while (ex.hasNext()) {
+                if (ex.hasNext(UNSIGNED_INT)) {
+                    next = ex.findInLine(UNSIGNED_INT);
+                    System.out.println("the number pushed " + next);
+                    num.push(new Integer(next));
+                } else {
+                    next = ex.findInLine(CHARACTER);
+                    c = next.charAt(0);
+                    System.out.println("The character: " + c);
+                    switch (c) {
+                        case ')':
+                            evaluateStackTops(opStack, num, bool);
+                            break;
+                        case '(':
+                            break;
+                        case '<':
+                        case 'L':
+                        case '>':
+                        case 'G':
+                        case 'E':
+                        case 'N':
+                        case '!':
+                        case 'A':
+                        case 'O':
+                            opStack.push(c);
+                            break;
+                        default:
+                            throw new IllegalArgumentException("Illegal operation");
+                    }
+                }
+            }
+            if (bool.size() != 1) {
+                throw new IllegalArgumentException("Illegal input expression");
+            }
+            System.out.println("the final result is: " + bool.peek());
+            return bool.pop();
+        }
+
+        private static boolean evalBoolean(int n1, int n2, char op) {
+            System.out.println("the numbers are: " + n1 + " , " + n2);
+            switch (op) {
+                case '<':
+                    return (n1 < n2);
+                case 'L':
+                    return (n1 <= n2);
+                case '>':
+                    return (n1 > n2);
+                case 'G':
+                    return (n1 >= n2);
+                case 'E':
+                    return (n1 == n2);
+                case 'N':
+                    return (n1 != n2);
+                default:
+                    throw new IllegalArgumentException("Invalid comparison operation");
+            }
+        }
+
+        private static void evaluateStackTops(Stack<Character> op, Stack<Integer> n, Stack<Boolean> b) {
+            if (op.size() < 1) throw new IllegalArgumentException();
+            char oper = op.pop();
+            System.out.println("the operation is: " + oper);
+            switch (oper) {
+                case '<':
+                case 'L':
+                case '>':
+                case 'G':
+                case 'E':
+                case 'N': {
+                    if (n.size() < 2) throw new IllegalArgumentException("Illegal expression (num stack underflow)");
+                    int operand2 = n.pop();
+                    int operand1 = n.pop();
+                    b.push(evalBoolean(operand1, operand2, oper));
+                    break;
+                }
+                case '!': {
+                    if (b.size() < 1)
+                        throw new IllegalArgumentException("Illegal expression (Booleans Stack Underflow)");
+                    b.push(!(b.pop()));
+                    break;
+                }
+                case 'A':
+                case 'O': {
+                    if (b.size() < 2)
+                        throw new IllegalArgumentException("Illegal expression (Booleans Stack Underflow)");
+                    boolean b2 = b.pop();
+                    boolean b1 = b.pop();
+                    if (oper == 'A')
+                        b.push((b1 && b2));
+                    else
+                        b.push((b1 || b2));
+                    break;
+                }
+                default:
+                    throw new IllegalArgumentException("Illegal operation (Unknown Character)");
+            }
+        }
+
+    }*/
+
+    // Q. 6 // done
+    /*public static double evaluatePostfix(String exp) {
+        Stack<Double> numStack = new Stack<>();
+        for (int i = 0; i < exp.length(); i++) {
+            char c = exp.charAt(i);
+            if (c == ' ')
+                continue;
+            if (Character.isDigit(c)) {
+                System.out.println(c);
+                numStack.push(new Double(c));
+            } else { //an operator is encountered
+                System.out.println(numStack.peek());
+                double operand2 = numStack.pop();
+                double operand1 = numStack.pop();
+                double result = evalArith(operand1, operand2, c);
+                numStack.push(result);
+            }
+        }
+        if (numStack.size() != 1) {
+            throw new IllegalArgumentException("Invalid Expression");
+        }
+        return numStack.pop();
+    }*/
+
+    //Q.7 as part  //  done
+    /*public static String convInToPostGen(String exp) { //this is for the case when the parenthesis need not to be fully
+        Stack<Character> opStack = new Stack<Character>();
+        String result = "";
+
+        if (!isBalanced(exp))
+            return "Not well formed";
+        for (int i = 0; i < exp.length(); i++) {
+            char c = exp.charAt(i);
+            if (c == ' ') continue;
+            if (Character.isLetterOrDigit(c))
+                result += c;
+            else if (c == LEFT_NORMAL)
+                opStack.push(c);
+            else if (c == RIGHT_NORMAL) {
+                while ((!opStack.isEmpty()) && (opStack.peek() != LEFT_NORMAL)) {
+                    result += opStack.pop();
+                }
+                if (!opStack.isEmpty()) {
+                    opStack.pop();
+                }
+            } else {
+                while (!opStack.isEmpty() && (opStack.peek() != LEFT_NORMAL) && prec(c) <= prec(opStack.peek())) {
+                    result += opStack.pop();
+                }
+                opStack.push(c);
+            }
+            System.out.println();
+            System.out.print(result + "   ");
+        }
+
+        while (!opStack.isEmpty()) {
+            if (opStack.peek() == LEFT_NORMAL) {
+                System.out.println(opStack.peek());
+                return "Invalid Expression";
+            }
+            result += opStack.pop();
+        }
+        return result;
+    }*/
+
+    //Q. 9  // NOT done
+    /*public static double evaluateInfixGen(String exp) { //not full parenthesis
+        Stack<Double> numStack = new Stack<>();
+        Stack<Character> opStack = new Stack<>();
+        Scanner input = new Scanner(exp);
+        String next;
+
+        for(int i=0; i <exp.length(); i++){
+            char c = exp.charAt(i);
+            if(Character.isDigit(c)){
+                int num = 0;     //if the number is not one digit
+                while(Character.isDigit(c)){
+                    num = num*10 + (c-'0');
+                    i++;
+                    if(i<exp.length()){
+                        c=exp.charAt(i);
+                    }else
+                        break;
+                }
+                i--;
+                numStack.push((double)num);
+            }else if(c=='(')
+                opStack.push(c);
+            else if(c==')'){
+                while(!opStack.isEmpty() ){
+                    evaluateStackTops(numStack, opStack);
+                }
+                opStack.pop();
+            }else{
+                while(!opStack.isEmpty()&& opStack.peek()!='(' &&  prec(c)<=prec(opStack.peek())){
+                    evaluateStackTops(numStack, opStack);
+                }
+                opStack.push(c);
+            }
+        }
+        */
+    /*while (input.hasNext()) {
+            if (input.hasNext(UNSIGNED_DOUBLE)) {
+                next = input.findInLine(UNSIGNED_DOUBLE);
+                numStack.push(new Double(next));
+            } else {
+                next = input.next(CHARACTER);
+                char c = next.charAt(0) ;
+                switch (c) {
+                    case '+':
+                    case '-':
+                    case '*':
+                    case '/':
+                        opStack.push(c);
+                        break;
+                    case ')':
+                        evaluateStackTops(numStack, opStack);
+                        break;
+                    case '(':
+                        break;
+                    default:
+                        throw new IllegalArgumentException("Illegal character");
+                }
+            }
+        }*/
+    /*
+
+        if (numStack.size() != 1) {
+            throw new IllegalArgumentException("Illegal input expression!");
+        }
+        return numStack.pop();
+    }*/
+
+    //Q.12 // done
+    /*public static <E> void displayTopBottom(LinkedStack<E> s) {
+        if (s.isEmpty()) return;
+        E x = s.pop();
+        System.out.print(x + " ");
+        displayTopBottom(s);
+        s.push(x);
+
+       /* //OR
+       if (s.isEmpty()) return;
+        Node<E> cursor = s.top;
+        for(; cursor != null; cursor = cursor.getLink())
+            System.out.print(cursor.getData() + " ");//
+    }*/
+
+
+    //Q.13  // done
+   /* public static <E> boolean compareStacks(LinkedStack<E> s1, LinkedStack<E> s2) {
+        if (s1 == null || s2 == null) return false;
+
+        if (s1.isEmpty() && s2.isEmpty()) return true;
+
+        if (s1.size() != s2.size()) return false;
+
+        Node<E> ptr1 = s1.top, ptr2 = s2.top;
+
+        while (ptr1 != null) {
+            if (ptr1.getData() != ptr2.getData()) return false;
+            ptr1 = ptr1.getLink();
+            ptr2 = ptr2.getLink();
+        }
+        return true;
+    }*/
+
+
+    // Q.14 // done
+    /*public static boolean palindrome(String exp) {
+        LinkedStack<Character> s1 = new LinkedStack<>();
+        LinkedStack<Character> temp;
+        LinkedStack<Character> s2 = new LinkedStack<>();
+
+        for (int i = 0; i < exp.length(); i++)
+            s1.push(exp.charAt(i));
+        temp = s1.clone();
+
+        while (!temp.isEmpty())
+            s2.push(temp.pop());
+
+        return compareStacks(s1, s2);
+    }*/
+
+    //Q.15   //done
+    /*public void flip() {
+        LinkedStack<E> temp;
+        temp = this.clone();
+        while (!temp.isEmpty()) {
+            E t = temp.pop();
+            this.push(t);
+        }
+    }*/
+}
