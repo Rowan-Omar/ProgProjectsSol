6f13b5fc236c35414e2e4ae321e122e331423292
Rowan Omar <61714846+Rowan-Omar@users.noreply.github.com>
2022-01-23 04:11:21 +0200

    Update Ch4_LinkedList.java

--- src/Ch4_LinkedList.java
+++ src/Ch4_LinkedList.java
@@ -1,19 +1,19 @@
 public class Ch4_LinkedList {
-   // Q. 2 // done
+    // Q. 2 // done
     /* public static <E> void removeDuplicates(Node<E> head) {
-        Node<E> current = head, dummy, temp2;
+        Node<E> current = head, prev, next;
 
-        while (current != null && current.getLink() != null) {
-            dummy = current;
-            temp2 = current.getLink();
-            while (temp2 != null) {
-                if (current.getData() == temp2.getData()) {
-                    dummy.setLink(temp2.getLink());
+        while (current != null && current.link != null) {
+            prev = current;
+            next = current.link;
+            while (next != null) {
+                if (current.data == next.data) {
+                    prev.link = next.link;
                 } else
-                    dummy = temp2;
-                temp2 = temp2.getLink();
+                    prev = next;
+                temp = temp.link;
             }
-            current = current.getLink();
+            current = current.link;
         }
     }*/
 
@@ -45,6 +45,22 @@ public class Ch4_LinkedList {
         }
         answer = prev;
         return answer;
+
+        OR
+Stack<Node<E>> stack = new Stack<Node<E>>();
+        Node<E> temp = head, reversedHead;
+        while (temp.link != null) {
+            stack.push(temp);
+            temp = temp.link;
+        }
+        reversedHead = temp;
+        while (!stack.isEmpty()) {
+            temp.link = stack.pop();
+            temp = temp.link;
+        }
+        temp.link = null;
+        return reversedHead;
+
     }*/
 
 
@@ -122,7 +138,7 @@ public class Ch4_LinkedList {
         while (current != null) {
             next = current.link;
             while (next != null) {
-                if (current.data > next.data) {
+                if (current.data > next.data) { //need to swap
                     temp = current.data;
                     current.data = next.data;
                     next.data = temp;
@@ -131,58 +147,167 @@ public class Ch4_LinkedList {
             }
             current = current.link;
         }
-        return null;
+        return head;
     }*/
 
 
-    //Q.8  // NOT done
-    /*  public void subtract(LinkedList<Integer> Y) {
-        LinkedList<Integer> temp;
-        temp = (LinkedList<Integer>) this.clone();
-        int max = -1, max2 = -1;
 
-        while (temp.head != null) {
-            if (max < temp.head.getData())
-                max = temp.head.getData();
-            temp.head = temp.head.getLink();
-        }
-        temp = (LinkedList<Integer>) this.clone();
-        while (temp.head != null) {
-            if (max2 < temp.head.getData())
-                max2 = temp.head.getData();
-            temp.head = temp.head.getLink();
-        }
-        int[] myArr = new int[max + 1];
-        int[] myArr2 = new int[max2 + 1];
+    //Q.8  // done
+    /*public IntLinkedBag subtract(IntLinkedBag Y) {
+        Node<Integer> tempHead = this.head; //for the first list (x)
+        Node<Integer> tempHead2 = Y.head; // to hold the second list
 
-        temp = (LinkedList<Integer>) this.clone();
+        int maxVal = -1, maxVal2 = -1; //to hold the biggest number in each list
 
-        while (temp.head != null) {
-            if (myArr[temp.head.getData()] == 0)
-                myArr[temp.head.getData()] = temp.countOccurrences(temp.head.getData());
+        IntLinkedBag newBag = new IntLinkedBag();
 
-            temp.head = temp.head.getLink();
-        }
-        LinkedList<Integer> temp2=(LinkedList<Integer>) this.clone();
-
-        while (temp2.head != null) {
-            if (myArr2[temp2.head.getData()] == 0)
-                myArr2[temp2.head.getData()] = temp2.countOccurrences(temp2.head.getData());
-
-            temp2.head = temp2.head.getLink();
-        }
-        for(int i=0; i <= (max<max2 ? max: max2); i++){
-            myArr[i] = myArr[i] - myArr[i];
+        while (tempHead != null) {
+            if (maxVal < tempHead.getData())
+                maxVal = tempHead.getData();
+            tempHead = tempHead.getLink();
         }
 
-        LinkedList<Integer> newBag = new LinkedList<>();
-        for (int i=0; i<=max; i++){
-            for(int j = 1; j<=myArr[i]; j++){
-                if(myArr[i]>0)
-                    //addLast(myArr[i]);
+        while (tempHead2 != null) {
+            if (maxVal2 < tempHead2.getData())
+                maxVal2 = tempHead2.getData();
+            tempHead2 = tempHead2.getLink();
+        }
+
+        int[] arr1 = new int[maxVal + 1]; //array to hold number of occurrence of each number and place it in the number's value
+        int[] arr2 = new int[maxVal2 + 1]; //for the second list numbers
+
+        tempHead = this.head;
+        while (tempHead != null) {
+            int tempData = tempHead.getData();
+            arr1[tempData] = this.countOccurrences(tempData);
+            tempHead = tempHead.getLink();
+        }
+
+        tempHead2 = Y.head;
+        while (tempHead2 != null) {
+            int temp2Data = tempHead2.getData();
+            arr2[temp2Data] = Y.countOccurrences(temp2Data);
+            tempHead2 = tempHead2.getLink();
+        }
+
+        //iterate to the min size among both arrays
+        for (int i = 0; i <= (maxVal < maxVal2 ? maxVal : maxVal2); i++) {
+            arr1[i] = arr1[i] - arr2[i];
+        }
+
+        for (int i = 0; i <= maxVal; i++) {
+            for (int j = 1; j <= arr1[i]; j++) {
+                if (arr1[i] > 0) {
+                    newBag.addLast(i);
+                }
             }
         }
+        return newBag;
     }*/
+
+    //Q.9 // done
+
+    /*//done
+    public void addFirst(E element) {
+        head = new Node<E>(element, head);
+        ;
+        cursor = head;
+        precursor = null;
+        if (manyNodes == 0)  //empty list
+            tail = head;
+        manyNodes++;
+    }
+
+    //done
+    public void addLast(E element) {
+        if (manyNodes == 0) {
+            head = tail = new Node<E>(element, null);
+            cursor = head;
+            precursor = null;
+        } else {
+            precursor = tail;
+            tail.addNodeAfter(element);
+            tail = tail.getLink();
+            cursor = tail;
+        }
+        manyNodes++;
+    }
+
+    //done
+    public E removeFirst() {
+        if (manyNodes == 0)
+            throw new IllegalStateException("There is no elements");
+        E removed = head.getData();
+        head = head.getLink();
+        cursor = head;
+        precursor = null;
+        manyNodes--;
+        if (manyNodes == 0)
+            tail = null;
+        return removed;
+    }
+
+    //done
+    public E removeLast(){
+        if(manyNodes == 0)
+            throw new IllegalStateException();
+        E removed = tail.getData();
+        precursor = head;
+        while(precursor.getLink() != tail)
+            precursor = precursor.getLink();
+        cursor = head;
+        precursor.setLink(null);
+        tail = precursor;
+        manyNodes--;
+        if(manyNodes == 0)
+            tail = head = cursor = precursor = null;
+        return removed;
+    }
+
+    //done
+    public void makeLastCurrent() {
+        if (manyNodes == 0) throw new IllegalStateException();
+        precursor = head;
+        while (precursor.getLink() != tail)
+            precursor = precursor.getLink();
+        cursor = tail;
+        System.out.println("cur " + cursor.getData() + " pre " + precursor.getData());
+    }
+
+    //done
+    public E getIth(int pos){
+        int i = 0;
+        Node<E> temp  = head;
+        while(temp!=null && i<pos)
+        {
+            temp = temp.getLink();
+            i++;
+        }
+        if(temp==null)
+            throw new IllegalArgumentException("Not found");
+        return temp.getData();
+    }
+
+    // done
+    public void makeIthCurrent(int pos) {
+        if (pos == 0) {
+            precursor = null;
+            cursor = head;
+            return;
+        }
+        int i = 1;
+        Node<E> temp = head;
+
+        while (temp != null && i < pos) {
+            temp = temp.getLink();
+            i++;
+        }
+        precursor = temp;
+        cursor = precursor.getLink();
+        //System.out.println("crs: " + cursor.getData() + " pre: " + precursor.getData());
+    }
+
+    */
 
 
 }
